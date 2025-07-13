75840f11f1c09f6edf650c87425e0162af47624f
ajitsharma356 <ajitsharma356@gmail.com>
2025-07-14 01:51:21 +0530

    Updated app.java

--- devops_lab4/src/main/java/com/DevOps/lab4/App.java
+++ devops_lab4/src/main/java/com/DevOps/lab4/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! from CI Triggers" );
+	System.out.println("Devops lab has complted");
     }
 }
